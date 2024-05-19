52108bb4f23d6038ab9939fc59fb46c81de3e922
= <=>
2024-05-19 12:00:54 +0300

    ++http services generated via openApi

---
+++ src/app/services/models/book-request.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface BookRequest {
+  authorName: string;
+  id?: number;
+  isShareable?: boolean;
+  isbn: string;
+  synopsis: string;
+  title: string;
+}
